26a29539b79870e777eaad12c62921875aa2d0c3
smileyface12349 <34768660+smileyface12349@users.noreply.github.com>
2023-01-27 14:24:48 +0000

    Some testing that doesn't work; Added TextMeshPro stuff

---
+++ Assets/Scripts/EquationTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EquationTest : MonoBehaviour
+{
+    public GameObject textInput;
+    public TMP_Text textInputText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textInputText = textInput.GetComponent<TextMeshPro>();
+        textInput = GameObject.FindWithTag("EquationInputBox");
+        Debug.Log(textInput);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textInputText.text = "Haha funny";
+        Debug.Log(gameObject.GetComponent<TextMeshPro>().text);
+        Debug.Log(textInput.GetComponent<TextMeshPro>().text);
+        gameObject.GetComponent<TextMeshPro>().text = textInput.GetComponent<TextMeshPro>().text;
+    }
+}

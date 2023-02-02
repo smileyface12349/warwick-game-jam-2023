808e524f7887b210634d012d1803789c59ef449b
LeilaDavis <Leila.Davis@warwick.ac.uk>
2023-02-02 21:57:41 +0000

    made some progress on following points

--- Assets/Scripts/FollowWaypoints.cs
+++ Assets/Scripts/FollowWaypoints.cs
@@ -5,26 +5,32 @@ using UnityEngine;
 public class FollowWaypoints : MonoBehaviour
 {
     public Vector2[] waypoints;
-    int currentWaypoint = 0;
-    public int speed = 3;
-    
+    public Vector2 position;
+    public float speed = 3.0f;
+    public int pointIndex = 0;
+    public bool currentlyMoving = false;
+    private Vector2 target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = waypoints[pointIndex];
+        position = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 currentPoint = waypoints[currentWaypoint];
-        if (Vector2.Distance(this.transform.position, currentPoint) < 2)
-            currentWaypoint++;
-        
-        if (currentWaypoint >= waypoints.Length)
-            currentWaypoint = 0;
+        float step = speed * Time.deltaTime;
+        if (pointIndex <= (waypoints.Length - 1)) {
+            target = waypoints[pointIndex];
+            position = gameObject.transform.position;
+            if (Vector2.Distance(target, position) == 0) {
+                pointIndex++;
+            }
 
-        this.transform.LookAt(waypoints[currentWaypoint]);
-        this.transform.Translate(0, 0, speed * Time.deltaTime);   
+            gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, (Vector2.SignedAngle((target - position), new Vector2(1, 0))));
+            transform.position = Vector2.MoveTowards(transform.position, target, step);
+        }
     }
 }

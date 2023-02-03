37e37f7bba6bf0ab647699a47f4bc6a7d07dd54f
smileyface12349 <josh.humphriss.signup@gmail.coom>
2023-02-03 18:43:29 +0000

    Graph plotting is now fully working, with a bounds check (but no continuity test)! Hooray!

--- Assets/Scripts/GraphHandler.cs
+++ Assets/Scripts/GraphHandler.cs
@@ -17,6 +17,7 @@ public class GraphHandler : MonoBehaviour
     private List<List<Vector3>> pointsList;
     private Func<float, float> theFunction;
     private Vector2 coordinatesBottomLeft;
+    private static float SILLY_NUMBER = -100000f;
     
     // Start is called before the first frame update
     private void Start()
@@ -52,52 +53,85 @@ public class GraphHandler : MonoBehaviour
         theFunction = expr.Compile<float, float>("x");
     }
 
+    private float InterpolatePoint(float y, float x1, float x2, float y1, float y2)
+    {
+        return x1 + (x2 - x1) * ((y - y1) / (y2 - y1));
+    }
+
     private List<List<Vector3>> DeterminePoints()
     {
         float increment = (topRight.x - bottomLeft.x) / nPoints;
         List<List<Vector3>> points = new List<List<Vector3>>();
         List<Vector3> currentPoints = new List<Vector3>();
-        Vector3 previousPoint = new Vector3(-1000, -1000);
+        Vector3 previousPoint = new Vector3(SILLY_NUMBER, SILLY_NUMBER);
         
         for (float x = bottomLeft.x; x < topRight.x+increment; x += increment)
         {
             bool valid = true;
-            float newX = -1000;
-            float newY = -1000;
+            bool startNew = false;
+            float newX = SILLY_NUMBER;
+            float newY = SILLY_NUMBER;
             float y = theFunction(x);
             
-            // Bounds Checks
-            Debug.Log("Checking bounds for (" + x + ", " + y + ")");
+            Debug.Log("Considering point (" + x + ", " + y + ")");
+            
+            // Check for OUT OF BOUNDS
             if (y > topRight.y) {  // ABOVE
                 Debug.Log("It's too high! " + topRight.y);
                 valid = false;
                 // Check if the previous point is valid - if not, we don't need to do anything
                 Debug.Log("Previous y: " + previousPoint.y);
-                if (previousPoint.y != -1000 && previousPoint.y <= topRight.y) {
+                if (previousPoint.y != SILLY_NUMBER && previousPoint.y < topRight.y && previousPoint.y > bottomLeft.y) {
                     // This is the first point to step out of bounds
                     // Want to add a point on the boundary
                     // To make it easier, we will add a point that is pretty close to where it should be, using linear interpolation
                     newY = topRight.y;
-                    newX = previousPoint.x + (x - previousPoint.x) * ((newY - previousPoint.y) / (y - previousPoint.y));
+                    newX = InterpolatePoint(newY, previousPoint.x, x, previousPoint.y, y);
                     Debug.Log(x);
                     Debug.Log(newX);
+                    startNew = true;
                 }
             }
             else if (y < bottomLeft.y) {  // BELOW
                 valid = false;
-                if (previousPoint.x != -1000 && previousPoint.y >= bottomLeft.y) {
+                if (previousPoint.x != SILLY_NUMBER && previousPoint.y > bottomLeft.y && previousPoint.y < topRight.y) {
                     // (see comments above)
                     // I split this into separate things as I thought it was a good idea but in hindsight it was stupid
                     newY = bottomLeft.y;
-                    newX = previousPoint.x + (x - previousPoint.x) * ((newY - previousPoint.y) / (y - previousPoint.y));
+                    newX = InterpolatePoint(newY, previousPoint.x, x, previousPoint.y, y);
+                    startNew = true;
+                }
+            }
+            // Bounds check passed - Now check if it is the first point to step into bounds
+            else
+            {
+                if (previousPoint.y != SILLY_NUMBER && (previousPoint.y > topRight.y || previousPoint.y != SILLY_NUMBER && previousPoint.y < bottomLeft.y))
+                {
+                    // Previous point was out of bounds
+                    // TODO: Could differentiate here to determine more reliably
+                    // Determine whether we have come in from the bottom or the top
+                    if (y >= bottomLeft.y + (topRight.y - bottomLeft.y) / 2)
+                    {
+                        newY = topRight.y;
+                        Debug.Log("Coming in from the top! New Y: " + newY);
+                    }
+                    else
+                    {
+                        Debug.Log("Coming in from the bottom!");
+                        newY = bottomLeft.y;
+                    }
+                    newX = InterpolatePoint(newY, previousPoint.x, x, previousPoint.y, y);
+                    Debug.Log("Adding the point: (" + newX + ", " + newY + ")");
+                    currentPoints.Add(new Vector3(GetXCoordinate(newX), GetYCoordinate(newY)));
                 }
             }
             
+            // TODO: Continuity check
+            
             float plotX = x;
             float plotY = y;
-            
-            // TODO: Countinuity check
-            if (!valid && newX != -1000 && newY != -1000)
+
+            if (!valid && newX != SILLY_NUMBER && newY != SILLY_NUMBER)
             {
                 plotX = newX;
                 plotY = newY;
@@ -108,14 +142,25 @@ public class GraphHandler : MonoBehaviour
                 
             if (valid) {
                 currentPoints.Add(point);
-                Debug.Log(point);
+                Debug.Log("Adding: " + point);
+            }
+
+            if (startNew)
+            {
+                points.Add(currentPoints);
+                currentPoints = new List<Vector3>();
             }
             
             previousPoint = new Vector2(plotX, plotY);
             
         }
-        Debug.Log(currentPoints.ToArray().Length);
-        points.Add(currentPoints);
+
+        Vector3[] currentPointsArray = currentPoints.ToArray();
+        if (currentPointsArray.Length > 0)
+        {
+            points.Add(currentPoints);
+        }
+        
         return points;
     }
 

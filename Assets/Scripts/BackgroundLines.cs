a53a764e548e69bddb6bc66299e0747af8ba0698
smileyface12349 <josh.humphriss.signup@gmail.coom>
2023-02-05 13:24:30 +0000

    Background lines now working properly for fractional start and end points on the coordinate grid

--- Assets/Scripts/BackgroundLines.cs
+++ Assets/Scripts/BackgroundLines.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,6 +18,8 @@ public class BackgroundLines : MonoBehaviour
 
     private float spacingX;
     private float spacingY;
+    private float offsetX;
+    private float offsetY;
 
     private int smallLineWidth;
     private int bigLineWidth;
@@ -44,8 +47,8 @@ public class BackgroundLines : MonoBehaviour
         //Top right is bottom left plus size
         canvasTopRig = (Vector2) canvasBotLef + containerSize;
         //Log
-        Debug.Log(canvasBotLef);
-        Debug.Log(canvasTopRig);
+        // Debug.Log(canvasBotLef);
+        // Debug.Log(canvasTopRig);
 
         //Access graph plotter
         gp = graph.GetComponent<GraphHandler>();
@@ -54,26 +57,46 @@ public class BackgroundLines : MonoBehaviour
         graphHeight  = gp.topRight.y - gp.bottomLeft.y;
 
         //Log
-        Debug.Log(canvasWidth);
-        Debug.Log(canvasHeight);
-        Debug.Log(graphWidth);
-        Debug.Log(graphHeight);
+        // Debug.Log(canvasWidth);
+        // Debug.Log(canvasHeight);
+        // Debug.Log(graphWidth);
+        // Debug.Log(graphHeight);
 
         //Determine space between lines
         spacingX = canvasWidth / graphWidth;
         spacingY = canvasHeight / graphHeight;
+        
+        // Determine offsets (if co-ordinates are not integers)
+        if (gp.bottomLeft.x == Math.Floor(gp.bottomLeft.x))
+        {
+            offsetX = 0;
+        } 
+        else
+        {
+            offsetX = (1 - (gp.bottomLeft.x - (float) Math.Floor(gp.bottomLeft.x))) * spacingX;
+        }
+
+        if (gp.bottomLeft.y == Math.Floor(gp.bottomLeft.y))
+        {
+            offsetY = 0;
+        }
+        else
+        {
+            offsetY = (1 - (gp.bottomLeft.y - (float) Math.Floor(gp.bottomLeft.y))) * spacingY;
+        }
+        
 
         //Central vector
         centre = new Vector2(canvasBotLef.x + ((float) 0.5) * canvasWidth, canvasBotLef.y + ((float) 0.5) * canvasHeight);
 
         //Summon small x lines
-        for (float i = 0; i <= canvasWidth; i = i + spacingX) {
-            Instantiate(smallVer, new Vector2(canvasBotLef.x + i, centre.y), Quaternion.identity);    
+        for (float i = 0; i <= canvasWidth; i += spacingX) {
+            Instantiate(smallVer, new Vector2(offsetX + canvasBotLef.x + i, centre.y), Quaternion.identity);    
         }
 
         //Summon small y lines
         for (float i = 0; i <= canvasHeight; i = i + spacingY) {
-            Instantiate(smallHor, new Vector2(centre.x, canvasBotLef.y + i), Quaternion.identity);    
+            Instantiate(smallHor, new Vector2(centre.x, offsetY + canvasBotLef.y + i), Quaternion.identity);    
         }
 
         //Make lines that pass through origin
